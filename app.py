b9a6740c81e32979edc3a3e1952cee5afca022f2
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2024-07-27 23:09:17 -0300

    FEAT(app): Criando app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+db = SQLAlchemy(app)
+
+from views.views import *
+
+if __name__ == '__main__':
+    app.run(debug=True)
