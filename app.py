5151e4cc8956c107a111001b4479da510e822bd0
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-07-30 21:50:29 -0300

    FEAT(app): Importando modulos

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
 from views.views import *
+from views.views_companies import *
+from views.views_users import *
 
 if __name__ == '__main__':
     app.run(debug=True)
