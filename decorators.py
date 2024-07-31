fd86b9757d1470147888c9d2986b2473d1831515
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-07-31 01:37:03 -0300

    FEAT(decorators): Criando função login_required

---
+++ decorators.py
@@ -0,0 +1,11 @@
+from functools import wraps
+from flask import redirect, url_for, session, flash
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            flash('Você precisa estar logado para acessar esta página.', 'danger')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
