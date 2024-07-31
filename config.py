c81fa4c807da11df00b98057e81f79e3f24ae762
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-07-30 23:51:34 -0300

    FEAT(config): Adicionando variaveis de config

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-SECRET_KEY = 'flaskUpflow'
+SECRET_KEY = 'flaskDbUpflow'
 SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
         SGBD = 'mysql+mysqlconnector',
         usuario = 'root',
         senha = 'admin',
         servidor = 'localhost',
-        database = 'upflow'
+        database = 'db_upflow'
     )
 UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + "/uploads"
