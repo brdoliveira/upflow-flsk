ceb9dc7fb83892d1c3acb017d641e35703db691d
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-01 00:00:00 -0300

    FEAT(decorators): Criando método permission_required

--- decorators.py
+++ decorators.py
@@ -1,5 +1,6 @@
 from functools import wraps
 from flask import redirect, url_for, session, flash
+from enums import PermissionLevel
 
 def login_required(f):
     @wraps(f)
@@ -9,3 +10,15 @@ def login_required(f):
             return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
+
+def permission_required(required_permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            user_permission = session.get('permission_level_id')
+            if user_permission is None or user_permission > required_permission.value:
+                flash('Você não tem permissão para acessar esta página.', 'danger')
+                return redirect(url_for('home'))
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
