da1d8c79f0604ea666022b0922804bb68529fbdb
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-02 20:28:04 -0300

    FEAT(decorators): Documentando codigo

--- decorators.py
+++ decorators.py
@@ -3,6 +3,17 @@ from flask import redirect, url_for, session, flash
 from enums import PermissionLevel
 
 def login_required(f):
+    """
+    Decorador para verificar se o usuário está logado antes de acessar uma rota.
+
+    Se o usuário não estiver logado, redireciona para a página de login e exibe uma mensagem de aviso.
+    
+    Parâmetros:
+    - f: Função a ser decorada.
+    
+    Retorna:
+    - Função decorada que verifica a autenticação do usuário.
+    """
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not session.get('logged_in'):
@@ -12,6 +23,17 @@ def login_required(f):
     return decorated_function
 
 def permission_required(required_permission):
+    """
+    Decorador para verificar se o usuário tem a permissão necessária para acessar uma rota.
+
+    Se o usuário não tiver a permissão necessária, redireciona para a página inicial e exibe uma mensagem de aviso.
+    
+    Parâmetros:
+    - required_permission: Nível de permissão necessário para acessar a rota.
+    
+    Retorna:
+    - Decorador que verifica o nível de permissão do usuário.
+    """
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
