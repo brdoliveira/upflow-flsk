56f998adec66801f8b8e91dd35a2a85c64773c2f
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-02 20:25:53 -0300

    FEAT(views_companies): Documentando codigo

--- views/views_companies.py
+++ views/views_companies.py
@@ -2,19 +2,39 @@ from flask import render_template, request, redirect, url_for, flash
 from app import app, db
 from models import Company
 from decorators import login_required, permission_required
-from enums import PermissionLevel
+from enums import PermissionLevel as pl
 
+# Rota para listar empresas
 @app.route('/companies', methods=['GET'])
 @login_required
-@permission_required(PermissionLevel.ADMIN)
+@permission_required(pl.ADMIN)
 def list_companies():
+    """
+    Lista todas as empresas.
+    
+    Requer:
+    - Usuário autenticado.
+    - Permissão de ADMIN.
+    """
     companies = Company.query.all()
     return render_template('list_companies.html', companies=companies)
 
+# Rota para adicionar empresa
 @app.route('/companies/add', methods=['GET', 'POST'])
 @login_required
-@permission_required(PermissionLevel.ADMIN)
+@permission_required(pl.ADMIN)
 def add_company():
+    """
+    Adiciona uma nova empresa.
+    
+    Métodos:
+    - GET: Renderiza o formulário de empresa.
+    - POST: Processa o envio do formulário e salva a nova empresa no banco de dados.
+    
+    Requer:
+    - Usuário autenticado.
+    - Permissão de ADMIN.
+    """
     if request.method == 'POST':
         name = request.form['name']
         cnpj = request.form['cnpj']
@@ -31,10 +51,22 @@ def add_company():
     
     return render_template('company_form.html', company=None)
 
+# Rota para editar empresa
 @app.route('/companies/edit/<int:company_id>', methods=['GET', 'POST'])
 @login_required
-@permission_required(PermissionLevel.ADMIN)
+@permission_required(pl.ADMIN)
 def edit_company(company_id):
+    """
+    Edita uma empresa existente.
+    
+    Métodos:
+    - GET: Renderiza o formulário de empresa com dados existentes.
+    - POST: Processa o envio do formulário e atualiza a empresa no banco de dados.
+    
+    Requer:
+    - Usuário autenticado.
+    - Permissão de ADMIN.
+    """
     company = Company.query.get_or_404(company_id)
     if request.method == 'POST':
         company.Name = request.form['name']
@@ -50,10 +82,21 @@ def edit_company(company_id):
     
     return render_template('company_form.html', company=company)
 
+# Rota para deletar empresa
 @app.route('/companies/delete/<int:company_id>', methods=['POST'])
 @login_required
-@permission_required(PermissionLevel.ADMIN)
+@permission_required(pl.ADMIN)
 def delete_company(company_id):
+    """
+    Deleta uma empresa.
+    
+    Métodos:
+    - POST: Processa a deleção da empresa.
+    
+    Requer:
+    - Usuário autenticado.
+    - Permissão de ADMIN.
+    """
     company = Company.query.get_or_404(company_id)
     db.session.delete(company)
     db.session.commit()
