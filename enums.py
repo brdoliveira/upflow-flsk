585a65c7eefb107624aa377fc1858bb8d490237f
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-02 20:28:09 -0300

    FEAT(enums): Documentando codigo

--- enums.py
+++ enums.py
@@ -1,6 +1,15 @@
 from enum import Enum
 
 class PermissionLevel(Enum):
+    """
+    Enumeração para representar os níveis de permissão dos usuários.
+    
+    Atributos:
+    - ADMIN: Nível de permissão de administrador (valor 1).
+    - EDITOR: Nível de permissão de editor (valor 2).
+    - VIEWER: Nível de permissão de visualizador (valor 3).
+    - GUEST: Nível de permissão de convidado (valor 4).
+    """
     ADMIN = 1
     EDITOR = 2
     VIEWER = 3
