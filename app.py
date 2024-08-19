b49c36a2e39d1133cb141958e0f98afa1421df42
brdoliveira <brunoribeirooliveira64@gmail.com>
2024-08-19 05:15:34 -0300

    FEAT(app): Adicionando caminho absoluto

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from tools.pdf_classifier import PDFClassifier
+
+path_abs = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 pdf_classifier = PDFClassifier()
-pdf_classifier.load_pdfs(r"C:\Users\brufe\OneDrive\Área de Trabalho\Git\upflow-flsk\uploads\ml_files\imposto_de_renda", label=2)
-pdf_classifier.load_pdfs(r"C:\Users\brufe\OneDrive\Área de Trabalho\Git\upflow-flsk\uploads\ml_files\nota_fiscal", label=1)
-pdf_classifier.load_pdfs(r"C:\Users\brufe\OneDrive\Área de Trabalho\Git\upflow-flsk\uploads\ml_files\boleto", label=0)
+pdf_classifier.load_pdfs(f"{path_abs}/uploads/ml_files/imposto_de_renda", label=2)
+pdf_classifier.load_pdfs(f"{path_abs}/uploads/ml_files/nota_fiscal", label=1)
+pdf_classifier.load_pdfs(f"{path_abs}/uploads/ml_files/boleto", label=0)
 pdf_classifier.train_model()
 
 from views.views import *
